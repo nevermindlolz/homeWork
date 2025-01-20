1be0df4f1928658ef1c0eaab60ed41014b25afd5
Numlocked <nevermindlolz@gmail.com>
2025-01-20 22:36:04 +0300

    newJulHW

--- hello_world/newJuliusHW1.js
+++ hello_world/newJuliusHW1.js
@@ -1,7 +1,7 @@
 function Task(id, title) {
     this.id = id;
     this.title = title;
-    this.isCompleted = true
+    this.isCompleted = false
 }
 
 Task.prototype.toggleComplete = function () {
@@ -16,11 +16,40 @@ function User(id, name) {
 }
 
 User.prototype.addTask = function (task) {
-    console.log('задача была добавлена')
+    console.log(`задача "${task.title}" была добавлена пользователю ${this.name}`)
     return this.tasks.push(task)
 }
 
 User.prototype.getTasks = function () {
+    console.log(`\nсписок всех задач:`)
+    console.log(this.tasks)
     return this.tasks
+
 }
+
+User.prototype.removeTask = function (taskId) {
+    console.log(`задача "${taskId.title}" была удалена`)
+    return this.tasks.filter(task => task.id)
+}
+
+const task1 = new Task(1, 'Помыть попу')
+const task2 = new Task(2, 'Побрить Жопу')
+const task3 = new Task(3, 'сыграть в доту')
+
+const user1 = new User(23, 'Julik')
+
+user1.addTask(task1)
+user1.addTask(task2)
+user1.addTask(task3)
+console.log(`\nОШИБКА!. что-то пошло не так...\n`)
+user1.removeTask(task1)
+
+user1.getTasks()
+
+
+
+
+
+
+
 
